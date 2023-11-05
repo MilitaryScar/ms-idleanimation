f6abf5ee625706f64f39686c30935330c7b7c940
MilitaryScar <99544566+MilitaryScar@users.noreply.github.com>
2023-11-05 15:55:13 +0000

    Update client.lua - Added the ability to have more then one animation

--- client.lua
+++ client.lua
@@ -1,9 +1,18 @@
 local isIdlePlaying = false
 local lastActionTime = 0
 local idleTimeout = 30000
-local currentAnimDict = "move_m@generic_idles@std" -- Default animation dictionary for male characters
-local alternateAnimDict = "move_f@generic_idles@std" -- Default animation dictionary for female characters
+local maleAnimDicts = {
+    "move_m@generic_idles@std",
+    "additional_male_anim_dict_1",
+    "additional_male_anim_dict_2"
+}
+local femaleAnimDicts = {
+    "move_f@generic_idles@std",
+    "additional_female_anim_dict_1",
+    "additional_female_anim_dict_2"
+}
 local interval = 30000  -- Set your desired interval in milliseconds
+local currentAnimIndex = 1
 
 Citizen.CreateThread(function()
     while true do
@@ -29,36 +38,16 @@ Citizen.CreateThread(function()
             end
 
             if GetGameTimer() - lastActionTime > idleTimeout and not isIdlePlaying then
-                local animDict = IsPedMale(playerPed) and currentAnimDict or alternateAnimDict
+                local animDicts = IsPedMale(playerPed) and maleAnimDicts or femaleAnimDicts
+                local animDict = animDicts[currentAnimIndex]
 
                 RequestAnimDict(animDict)
                 if HasAnimDictLoaded(animDict) then
                     TaskPlayAnim(playerPed, animDict, "idle", 8.0, -8, -1, 49, 0, false, false, false)
                     isIdlePlaying = true
-                end
-            end
-        end
-    end
-end)
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        local playerPed = PlayerPedId()
-
-        if DoesEntityExist(playerPed) and not IsEntityDead(playerPed) then
-            if IsControlPressed(0, 32) or IsControlPressed(0, 33) or IsControlPressed(0, 34) or IsControlPressed(0, 35) then
-
-                lastActionTime = GetGameTimer()
-            end
-            
-            if GetGameTimer() - lastActionTime > idleTimeout then
-                if not isIdlePlaying then
-                    RequestAnimDict("move_f@generic_idles@std")
-                    if HasAnimDictLoaded("move_f@generic_idles@std") then
-                        TaskPlayAnim(playerPed, "move_f@generic_idles@std", "idle", 8.0, -8, -1, 49, 0, false, false, false)
-                        isIdlePlaying = true
-                    end
+                    -- Cycle to the next animation dictionary
+                    currentAnimIndex = (currentAnimIndex % #animDicts) + 1
                 end
             end
         end
